7c9c6433315cf2d1e2974fb86c335762d5684332
Jonas Gorauskas <jgorauskas@gmail.com>
2022-12-27 15:34:33 -0800

    klog config

---
+++ src/main/kotlin/org/nixdork/klog/frameworks/config/KlogConfiguration.kt
@@ -0,0 +1,16 @@
+package org.nixdork.klog.frameworks.config
+
+import java.util.UUID
+
+data class KlogConfiguration(
+    val title: String,
+    val subtitle: String,
+    val domain: String,
+    val primaryAuthor: UUID,
+    val generator: String,
+    val icon: String,
+    val logo: String,
+    val style: String,
+    val rights: String,
+    val homeEntries: Int
+)
