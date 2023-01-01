3eb7582312f6377f4cd0d2e98bf6ac63664690d8
Jonas Gorauskas <jgorauskas@gmail.com>
2023-01-01 15:40:45 -0800

    db schema

--- src/main/resources/db/migration/V001__Klog_Initial.sql
+++ src/main/resources/db/migration/V001__Klog_Initial.sql
@@ -1,105 +1,107 @@
-create table if not exists link
-(
-    id       uuid primary key,
-    href     text not null,
-    title    text,
-    rel      text,
-    "type"   text,
-    hreflang text,
-    "length" bigint
-);
+create type klog_role as enum ('CONTRIBUTOR', 'ADMIN');
+-- contributor can edit own entries + person
+-- admin = full control
 
 create table if not exists person
 (
-    id     uuid primary key not null,
-    "name" text             not null,
-    email  text             not null,
-    uri    text
+    id            uuid primary key not null,
+    "name"        text             not null,
+    email         text             not null,
+    "hash"        text             not null, -- hex encoded
+    salt          text             not null, -- hex encoded
+    pwat          timestamp,                 -- password updated at
+    "role"        klog_role                 default 'CONTRIBUTOR',
+    uri           text,
+    avatar        text,
+    last_login_at timestamp,
+    created_at    timestamp        not null default now(),
+    updated_at    timestamp
 );
 
 create unique index if not exists person_email_uidx on person (email);
 
 create table if not exists "tag"
 (
-    id        uuid primary key not null,
-    term      text             not null, -- identifies the category
-    permalink text             not null,
-    "label"   text,                      -- human-readable label for display
-    scheme    text                       -- identifies the categorization scheme via a URI
+    id         uuid primary key not null,
+    term       text             not null, -- unique identifies the tag
+    permalink  text             not null,
+    created_at timestamp        not null default now(),
+    updated_at timestamp
 );
 
 create unique index if not exists tag_term_uidx on "tag" (term);
 
 create table if not exists entry
 (
-    id             uuid primary key not null,
-    slug           text             not null,
-    permalink      text             not null,
-    title          text             not null,
-    draft          bool             not null default true,
-    updated        timestamp        not null,
-    published      timestamp,
-    primary_author uuid references person on delete set null on update cascade,
-    "content"      text             not null,
-    summary        text,
-    rights         text
+    id           uuid primary key not null,
+    slug         text             not null,
+    permalink    text             not null,
+    title        text             not null,
+    draft        bool             not null default true,
+    created_at   timestamp        not null default now(),
+    updated_at   timestamp,
+    published_at timestamp,
+    author_id    uuid references person on delete set null on update cascade,
+    "content"    text             not null,
+    summary      text
 );
 
 create unique index if not exists entry_slug_uidx on entry (slug);
 
-create table if not exists "page"
-(
-    id             uuid primary key not null,
-    title          text             not null,
-    "path"         text             not null,
-    link_id        uuid references link on delete set null on update cascade,
-    updated        timestamp        not null,
-    published      timestamp,
-    primary_author uuid references person on delete set null on update cascade,
-    "content"      text             not null,
-    rights         text
-);
-
-create unique index if not exists page_path_uidx on "page" ("path");
-
-create table if not exists entry_to_link
-(
-    entry_id uuid references entry,
-    link_id  uuid references link,
-    primary key (entry_id, link_id)
-);
-
 create table if not exists entry_to_tag
 (
+    id       uuid primary key not null,
     entry_id uuid references entry,
-    tag_id   uuid references "tag",
-    primary key (entry_id, tag_id)
+    tag_id   uuid references "tag"
 );
 
-create table if not exists entry_to_contributor
+create index if not exists entry_to_tag_idx on entry_to_tag (entry_id, tag_id);
+
+create table if not exists entry_metadata
 (
-    entry_id  uuid references entry,
-    person_id uuid references person,
-    primary key (entry_id, person_id)
+    id         uuid primary key not null,
+    entry_id   uuid references entry on delete set null on update cascade,
+    "key"      text             not null,
+    "value"    text             not null,
+    created_at timestamp        not null default now(),
+    updated_at timestamp
 );
 
-create table if not exists entry_to_source
-(
-    entry_id  uuid references entry,
-    source_id uuid references link,
-    primary key (entry_id, source_id)
-);
+create index if not exists entry_metadata_key_idx on entry_metadata (entry, "key");
 
-create table if not exists page_to_link
-(
-    page_id uuid references page,
-    link_id uuid references link,
-    primary key (page_id, link_id)
-);
+create or replace function table_updated_trgfn()
+    returns trigger as
+$$
+begin
+    NEW.updated_at = now();
+    return NEW;
+end;
+$$ language 'plpgsql';
 
-create table if not exists page_to_contributor
-(
-    page_id   uuid references page,
-    person_id uuid references person,
-    primary key (page_id, person_id)
-);
+create trigger person_updated_at_trg
+    before update
+    on "person"
+    for each row
+    when ((pg_trigger_depth() < 1))
+    execute function table_updated_trgfn();
+
+create trigger tag_updated_at_trg
+    before update
+    on "tag"
+    for each row
+    when ((pg_trigger_depth() < 1))
+    execute function table_updated_trgfn();
+
+create trigger entry_updated_at_trg
+    before update
+    on entry
+    for each row
+    when ((pg_trigger_depth() < 1))
+    execute function table_updated_trgfn();
+
+create trigger entry_metadata_updated_at_trg
+    before update
+    on entry_metadata
+    for each row
+    when ((pg_trigger_depth() < 1))
+    execute function table_updated_trgfn();
