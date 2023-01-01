ff543ab6781a7e31b7bd349c9c9caf90466af30e
Jonas Gorauskas <jgorauskas@gmail.com>
2023-01-01 09:06:05 -0800

    db repo

--- src/main/kotlin/org/nixdork/klog/adapters/data/KlogRepository.kt
+++ src/main/kotlin/org/nixdork/klog/adapters/data/KlogRepository.kt
@@ -2,59 +2,44 @@ package org.nixdork.klog.adapters.data
 
 import org.nixdork.klog.adapters.model.EntryModel
 import org.nixdork.klog.adapters.model.EntryWrapperModel
-import org.nixdork.klog.adapters.model.LinkModel
-import org.nixdork.klog.adapters.model.PageModel
-import org.nixdork.klog.adapters.model.PageWrapperModel
 import org.nixdork.klog.adapters.model.PersonModel
 import org.nixdork.klog.adapters.model.TagModel
 import java.util.UUID
+import org.nixdork.klog.adapters.model.ArchiveWrapperModel
+import org.nixdork.klog.adapters.model.EntryMetadataModel
+import org.nixdork.klog.adapters.model.PersonPasswordModel
+import org.nixdork.klog.adapters.model.TagToEntriesWrapperModel
+import org.nixdork.klog.adapters.model.TagWrapperModel
 
 @Suppress("TooManyFunctions")
 interface KlogRepository {
-    fun getLastNEntries(n: Int): List<EntryWrapperModel>
+    fun getLastestNEntries(n: Int): EntryWrapperModel
 
-    fun getAllEntries(): List<EntryWrapperModel>
+    fun getAllEntries(): EntryWrapperModel
     fun getEntryBySlug(slug: String): EntryModel?
     fun getEntryById(id: UUID): EntryModel?
-    fun getEntriesByTag(term: String): List<EntryWrapperModel>
-
-    fun getAllPages(): List<PageWrapperModel>
-    fun getPageByPath(path: String): PageModel?
-    fun getPageById(id: UUID): PageModel?
+    fun getEntriesByTag(term: String): TagToEntriesWrapperModel
+    fun getArchivedEntries(): ArchiveWrapperModel
 
     fun getAllPeople(): List<PersonModel>
     fun getPersonByEmail(email: String): PersonModel?
     fun getPersonById(id: UUID): PersonModel?
-    fun getContributorsByEntryId(entryId: UUID): List<PersonModel>
-    fun getContributorsByPageId(pageId: UUID): List<PersonModel>
+    fun getPasswordByEmail(email: String): PersonPasswordModel?
+    fun verifyPassword(password: PersonPasswordModel): Boolean
 
-    fun getAllTags(): List<TagModel>
+    fun getAllTags(): TagWrapperModel
     fun getTagByTerm(term: String): TagModel?
     fun getTagById(id: UUID): TagModel?
-    fun getTagsByEntryId(entryId: UUID): List<TagModel>
 
-    fun getAllLinks(): List<LinkModel>
-    fun getLinkById(id: UUID): LinkModel?
-    fun getLinksByEntryId(entryId: UUID): List<LinkModel>
-    fun getSourcesByEntryId(entryId: UUID): List<LinkModel>
-    fun getLinksByPageId(pageId: UUID): List<LinkModel>
-
-    fun upsertEntry(entry: EntryModel): EntryModel
-    fun upsertLink(link: LinkModel): LinkModel
-    fun upsertPage(page: PageModel): PageModel
+    fun updateLastLogin(personId: UUID): PersonModel?
+    fun upsertEntry(authorId: UUID, entry: EntryModel): EntryModel
+    fun publishEntry(entryId: UUID): EntryModel
+    fun batchUpsertMetadata(entryId: UUID, metadata: List<EntryMetadataModel>)
     fun upsertPerson(person: PersonModel): PersonModel
+    fun upsertPassword(password: PersonPasswordModel): PersonModel
     fun upsertTag(tag: TagModel): TagModel
 
-    fun upsertEntryToLink(entryId: UUID, linkId: UUID)
-    fun upsertEntryToTag(entryId: UUID, tagId: UUID)
-    fun upsertEntryToContributor(entryId: UUID, contributorId: UUID)
-    fun upsertEntryToSource(entryId: UUID, sourceId: UUID)
-    fun upsertPageToLink(pageId: UUID, linkId: UUID)
-    fun upsertPageToContributor(entryId: UUID, contributorId: UUID)
-
     fun deleteEntry(entryId: UUID)
-    fun deletePage(pageId: UUID)
     fun deletePerson(personId: UUID)
-    fun deleteLink(linkId: UUID)
     fun deleteTag(tagId: UUID)
 }
--- src/main/kotlin/org/nixdork/klog/frameworks/data/ExposedKlogRepository.kt
+++ src/main/kotlin/org/nixdork/klog/frameworks/data/ExposedKlogRepository.kt
@@ -1,176 +1,206 @@
 package org.nixdork.klog.frameworks.data
 
-import org.nixdork.klog.adapters.data.KlogRepository
-import org.nixdork.klog.adapters.model.EntryModel
-import org.nixdork.klog.adapters.model.EntryWrapperModel
-import org.nixdork.klog.adapters.model.LinkModel
-import org.nixdork.klog.adapters.model.PageModel
-import org.nixdork.klog.adapters.model.PageWrapperModel
-import org.nixdork.klog.adapters.model.PersonModel
-import org.nixdork.klog.adapters.model.TagModel
-import org.springframework.stereotype.Component
-import java.util.UUID
+import org.jetbrains.exposed.sql.SortOrder
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.batchReplace
+import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
-import org.nixdork.klog.frameworks.data.dao.EntryToLink
-import org.nixdork.klog.frameworks.data.dao.EntryToTag
-import org.nixdork.klog.frameworks.data.dao.Link
-import org.nixdork.klog.frameworks.data.dao.Links
+import org.jetbrains.exposed.sql.update
+import org.nixdork.klog.adapters.data.KlogRepository
+import org.nixdork.klog.adapters.model.ArchiveModel
+import org.nixdork.klog.adapters.model.ArchiveWrapperModel
+import org.nixdork.klog.adapters.model.EntryMetadataModel
+import org.nixdork.klog.adapters.model.EntryModel
+import org.nixdork.klog.adapters.model.EntryWrapperModel
+import org.nixdork.klog.adapters.model.PersonModel
+import org.nixdork.klog.adapters.model.PersonPasswordModel
+import org.nixdork.klog.adapters.model.TagModel
+import org.nixdork.klog.adapters.model.TagToEntriesWrapperModel
+import org.nixdork.klog.adapters.model.TagWrapperModel
+import org.nixdork.klog.common.toOffsetDateTime
+import org.nixdork.klog.common.upsert
+import org.nixdork.klog.frameworks.data.dao.Entries
+import org.nixdork.klog.frameworks.data.dao.EntriesMetadata
+import org.nixdork.klog.frameworks.data.dao.EntriesToTags
+import org.nixdork.klog.frameworks.data.dao.Entry
+import org.nixdork.klog.frameworks.data.dao.People
+import org.nixdork.klog.frameworks.data.dao.Person
 import org.nixdork.klog.frameworks.data.dao.Tag
 import org.nixdork.klog.frameworks.data.dao.Tags
+import org.springframework.stereotype.Component
+import java.time.Instant
+import java.time.LocalDate
+import java.util.UUID
 
 @Suppress("TooManyFunctions")
 @Component
 class ExposedKlogRepository : KlogRepository {
-    override fun getLastNEntries(n: Int): List<EntryWrapperModel> =
+    override fun getLastestNEntries(n: Int): EntryWrapperModel =
         transaction {
+            Entries.select { Entries.draft eq false }
+                .orderBy(Entries.updatedAt to SortOrder.DESC)
+                .limit(n)
+                .map {
+                    Entry.wrapRow(it).toModel() }
+        }.toEntryWrapper()
 
+    override fun getAllEntries(): EntryWrapperModel =
+        transaction { Entry.all().map { it.toModel() } }.toEntryWrapper()
+
+    override fun getEntryBySlug(slug: String): EntryModel? =
+        transaction { Entry.find(Entries.slug eq slug).singleOrNull()?.toModel() }
+
+    override fun getEntryById(id: UUID): EntryModel? = transaction { Entry.findById(id)?.toModel() }
+
+    override fun getEntriesByTag(term: String): TagToEntriesWrapperModel =
+        transaction {
+            (Tags innerJoin EntriesToTags innerJoin Entries)
+                .select { Tags.term eq term }
+                .map { Pair(Tag.wrapRow(it), Entry.wrapRow(it)) }
+                .groupBy { it.first }
+                .let {
+                    TagToEntriesWrapperModel(
+                        tag = it.keys.first().toModel(),
+                        entries = it.values.flatMap { it.map { p -> p.second.toModel() } }
+                    )
+                }
         }
 
-    override fun getAllEntries(): List<EntryWrapperModel> {
-        TODO("Not yet implemented")
-    }
+    override fun getArchivedEntries(): ArchiveWrapperModel =
+        transaction {
+            Entries.select {
+                Entries.draft eq false
+            }.groupBy {
+                val year = it[Entries.publishedAt]?.toOffsetDateTime()?.year?.toString()?.padStart(4, '0')
+                val month = it[Entries.publishedAt]?.toOffsetDateTime()?.month?.toString()?.padStart(2, '0')
+                "$year-$month"
+            }.map {
+                val (year, month) = it.key.split('-')
+                ArchiveModel(
+                    date = LocalDate.of(year.toInt(), month.toInt(), 1),
+                    monthYear = it.key,
+                    entries = it.value.map { entry -> Entry.wrapRow(entry).toModel() }
+                )
+            }.let { ArchiveWrapperModel(archives = it) }
+        }
 
-    override fun getEntryBySlug(slug: String): EntryModel? {
-        TODO("Not yet implemented")
-    }
+    override fun getAllPeople(): List<PersonModel> = transaction { Person.all().map { it.toModel() } }
 
-    override fun getEntryById(id: UUID): EntryModel? {
-        TODO("Not yet implemented")
-    }
+    override fun getPersonByEmail(email: String): PersonModel? =
+        transaction { Person.find(People.email eq email).singleOrNull()?.toModel() }
 
-    override fun getEntriesByTag(term: String): List<EntryWrapperModel> {
-        TODO("Not yet implemented")
-    }
+    override fun getPersonById(id: UUID): PersonModel? = transaction { Person.findById(id)?.toModel() }
 
-    override fun getAllPages(): List<PageWrapperModel> {
-        TODO("Not yet implemented")
-    }
+    override fun getPasswordByEmail(email: String): PersonPasswordModel? =
+        transaction { Person.find(People.email eq email).singleOrNull()?.toPasswordModel() }
 
-    override fun getPageByPath(path: String): PageModel? {
-        TODO("Not yet implemented")
-    }
+    override fun verifyPassword(password: PersonPasswordModel): Boolean =
+        transaction { password.hash == Person.find(People.email eq password.email).singleOrNull()?.hash }
 
-    override fun getPageById(id: UUID): PageModel? {
-        TODO("Not yet implemented")
-    }
-
-    override fun getAllPeople(): List<PersonModel> {
-        TODO("Not yet implemented")
-    }
-
-    override fun getPersonByEmail(email: String): PersonModel? {
-        TODO("Not yet implemented")
-    }
-
-    override fun getPersonById(id: UUID): PersonModel? {
-        TODO("Not yet implemented")
-    }
-
-    override fun getContributorsByEntryId(entryId: UUID): List<PersonModel> {
-        TODO("Not yet implemented")
-    }
-
-    override fun getContributorsByPageId(pageId: UUID): List<PersonModel> {
-        TODO("Not yet implemented")
-    }
-
-    override fun getAllTags(): List<TagModel> = transaction { Tag.all().map { it.toModel() } }
+    override fun getAllTags(): TagWrapperModel = transaction { TagWrapperModel(Tag.all().map { it.toModel() }) }
 
     override fun getTagByTerm(term: String): TagModel? =
         transaction { Tag.find(Tags.term eq term).singleOrNull()?.toModel() }
 
-
     override fun getTagById(id: UUID): TagModel? = transaction { Tag.findById(id)?.toModel() }
 
-    override fun getTagsByEntryId(entryId: UUID): List<TagModel> {
-        val tagIds = EntryToTag.select { EntryToTag.entryId eq entryId }.map { it[EntryToTag.tagId].value }
-        return Tags.select { Tags.id inList tagIds }.map { Tag.wrapRow(it).toModel() }
-    }
-
-    override fun getAllLinks(): List<LinkModel> = transaction { Link.all().map { it.toModel() } }
-
-    override fun getLinkById(id: UUID): LinkModel? = transaction { Link.findById(id)?.toModel() }
-
-    override fun getLinksByEntryId(entryId: UUID): List<LinkModel> =
+    override fun updateLastLogin(personId: UUID): PersonModel? =
         transaction {
-            val linkIds = EntryToLink.select { EntryToLink.entryId eq entryId }.map { it[EntryToLink.linkId].value }
-            Links.select { Links.id inList linkIds }.map { Link.wrapRow(it).toModel() }
+            People.update({ People.id eq personId }) { it[lastLoginAt] = Instant.now() }
+            Person.findById(personId)?.toModel()
         }
 
-
-    override fun getSourcesByEntryId(entryId: UUID): List<LinkModel> =
+    override fun upsertEntry(authorId: UUID, entry: EntryModel): EntryModel =
         transaction {
-            val sourceIds = EntryToLink.select { EntryToLink.entryId eq entryId }.map { it[EntryToLink.linkId].value }
-            Links.select { Links.id inList sourceIds }.map { Link.wrapRow(it).toModel() }
+            Entries.upsert {
+                it[id] = entry.id
+                it[slug] = entry.slug
+                it[permalink] = entry.permalink
+                it[title] = entry.title
+                it[draft] = entry.draft
+                it[primaryAuthor] = authorId
+                it[content] = entry.content
+                it[summary] = entry.summary
+            }.resultedValues!!
+                .single()
+                .let {
+                    Entry.wrapRow(it).toModel()
+                }
         }
 
-    override fun getLinksByPageId(pageId: UUID): List<LinkModel> {
-        TODO("Not yet implemented")
+    override fun publishEntry(entryId: UUID): EntryModel =
+        transaction {
+            Entries.upsert {
+                it[id] = entryId
+                it[draft] = false
+                it[publishedAt] = Instant.now()
+            }.resultedValues!!
+                .single()
+                .let {
+                    Entry.wrapRow(it).toModel()
+                }
+        }
+
+    override fun batchUpsertMetadata(entryId: UUID, metadata: List<EntryMetadataModel>) {
+        EntriesMetadata.batchReplace(metadata) {
+            this[EntriesMetadata.id] = it.id
+            this[EntriesMetadata.key] = it.key
+            this[EntriesMetadata.value] = it.value
+            this[EntriesMetadata.entry] = entryId
+        }
     }
 
-    override fun upsertEntry(entry: EntryModel): EntryModel {
-        TODO("Not yet implemented")
-    }
+    override fun upsertPerson(person: PersonModel): PersonModel =
+        transaction {
+            People.upsert {
+                it[id] = person.id
+                it[email] = person.email
+                it[name] = person.name
+                it[role] = person.role
+                it[uri] = person.uri
+                it[avatar] = person.avatar
+            }.resultedValues!!
+                .single()
+                .let {
+                    Person.wrapRow(it).toModel()
+                }
+        }
 
-    override fun upsertLink(link: LinkModel): LinkModel {
-        TODO("Not yet implemented")
-    }
+    override fun upsertPassword(password: PersonPasswordModel): PersonModel =
+        transaction {
+            People.upsert {
+                it[id] = password.id
+                it[email] = password.email
+                it[hash] = password.hash
+                it[salt] = password.salt
+                it[role] = password.role
+                it[pwat] = Instant.now()
+            }.resultedValues!!
+                .single()
+                .let {
+                    Person.wrapRow(it).toModel()
+                }
+        }
 
-    override fun upsertPage(page: PageModel): PageModel {
-        TODO("Not yet implemented")
-    }
+    override fun upsertTag(tag: TagModel): TagModel =
+        transaction {
+            Tags.upsert {
+                it[id] = tag.id
+                it[term] = tag.term
+                it[permalink] = tag.permalink
+            }.resultedValues!!
+                .single()
+                .let {
+                    Tag.wrapRow(it).toModel()
+                }
+        }
 
-    override fun upsertPerson(person: PersonModel): PersonModel {
-        TODO("Not yet implemented")
-    }
+    override fun deleteEntry(entryId: UUID) { transaction { Entries.deleteWhere { Entries.id eq entryId } } }
 
-    override fun upsertTag(tag: TagModel): TagModel {
-        TODO("Not yet implemented")
-    }
+    override fun deletePerson(personId: UUID) { transaction { People.deleteWhere { People.id eq personId } } }
 
-    override fun upsertEntryToLink(entryId: UUID, linkId: UUID) {
-        TODO("Not yet implemented")
-    }
+    override fun deleteTag(tagId: UUID) { transaction { Tags.deleteWhere { Tags.id eq tagId } } }
 
-    override fun upsertEntryToTag(entryId: UUID, tagId: UUID) {
-        TODO("Not yet implemented")
-    }
-
-    override fun upsertEntryToContributor(entryId: UUID, contributorId: UUID) {
-        TODO("Not yet implemented")
-    }
-
-    override fun upsertEntryToSource(entryId: UUID, sourceId: UUID) {
-        TODO("Not yet implemented")
-    }
-
-    override fun upsertPageToLink(pageId: UUID, linkId: UUID) {
-        TODO("Not yet implemented")
-    }
-
-    override fun upsertPageToContributor(entryId: UUID, contributorId: UUID) {
-        TODO("Not yet implemented")
-    }
-
-    override fun deleteEntry(entryId: UUID) {
-        TODO("Not yet implemented")
-    }
-
-    override fun deletePage(pageId: UUID) {
-        TODO("Not yet implemented")
-    }
-
-    override fun deletePerson(personId: UUID) {
-        TODO("Not yet implemented")
-    }
-
-    override fun deleteLink(linkId: UUID) {
-        TODO("Not yet implemented")
-    }
-
-    override fun deleteTag(tagId: UUID) {
-        TODO("Not yet implemented")
-    }
+    private fun List<EntryModel>.toEntryWrapper(): EntryWrapperModel = EntryWrapperModel(entries = this)
 }
