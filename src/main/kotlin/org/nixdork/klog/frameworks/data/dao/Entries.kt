be53eb91abcf5fddabaad76fdf197fd1fbc45263
Jonas Gorauskas <jgorauskas@gmail.com>
2023-01-01 15:53:09 -0800

    entries dao

--- src/main/kotlin/org/nixdork/klog/frameworks/data/dao/Entries.kt
+++ src/main/kotlin/org/nixdork/klog/frameworks/data/dao/Entries.kt
@@ -4,20 +4,22 @@ import org.jetbrains.exposed.dao.UUIDEntity
 import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.UUIDTable
-import org.jetbrains.exposed.sql.kotlin.datetime.timestamp
+import org.jetbrains.exposed.sql.javatime.timestamp
+import org.nixdork.klog.adapters.model.EntryModel
+import org.nixdork.klog.common.toOffsetDateTime
 import java.util.UUID
 
 object Entries : UUIDTable(name = "entry") {
     val slug = text("slug")
-    val title = text("title")
     val permalink = text("permalink")
+    val title = text("title")
     val draft = bool("draft")
-    val updated = timestamp("updated")
-    val published = timestamp("published").nullable()
-    val primaryAuthor = uuid("primary_author")
+    val createdAt = timestamp("created_at")
+    val updatedAt = timestamp("updated_at").nullable()
+    val publishedAt = timestamp("published_at").nullable()
+    val primaryAuthor = reference("author_id", People)
     val content = text("content")
     val summary = text("summary").nullable()
-    val rights = text("rights").nullable()
 }
 
 class Entry(id: EntityID<UUID>): UUIDEntity(id) {
@@ -27,10 +29,30 @@ class Entry(id: EntityID<UUID>): UUIDEntity(id) {
     var title by Entries.title
     var permalink by Entries.permalink
     var draft by Entries.draft
-    var updated by Entries.updated
-    var published by Entries.published
-    var primaryAuthor by Entries.primaryAuthor
+    var createdAt by Entries.createdAt
+    var updatedAt by Entries.updatedAt
+    var publishedAt by Entries.publishedAt
+    var primaryAuthor by Person referencedOn Entries.primaryAuthor
     var content by Entries.content
     var summary by Entries.summary
-    var rights by Entries.rights
+
+    var tags by Tag via EntriesToTags
+    val metadata by EntryMetadata referrersOn EntriesMetadata.entry
+
+    fun toModel(): EntryModel =
+        EntryModel(
+            id = this.id.value,
+            title = this.title,
+            slug = this.slug,
+            permalink = this.permalink,
+            draft = this.draft,
+            createdAt = this.createdAt.toOffsetDateTime(),
+            updatedAt = this.updatedAt?.toOffsetDateTime(),
+            publishedAt = this.publishedAt?.toOffsetDateTime(),
+            primaryAuthor = this.primaryAuthor.toModel(),
+            content = this.content,
+            summary = this.summary,
+            tags = this.tags.map { it.toModel() },
+            metadata = this.metadata.map { it.toModel() }
+        )
 }
