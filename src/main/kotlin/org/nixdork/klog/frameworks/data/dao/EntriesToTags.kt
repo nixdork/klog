854d0befc1fe921b7421f4930086ab61816aec65
Jonas Gorauskas <jgorauskas@gmail.com>
2023-01-01 17:26:24 -0800

    entry to tag dao

--- src/main/kotlin/org/nixdork/klog/frameworks/data/dao/EntriesToTags.kt
+++ src/main/kotlin/org/nixdork/klog/frameworks/data/dao/EntriesToTags.kt
@@ -2,7 +2,7 @@ package org.nixdork.klog.frameworks.data.dao
 
 import org.jetbrains.exposed.dao.id.UUIDTable
 
-object EntryToTag : UUIDTable("entry_to_tag") {
-    val entryId = reference("entry_id", Entries)
-    val tagId = reference("tag_id", Tags)
+object EntriesToTags : UUIDTable("entry_to_tag") {
+    val entry = reference("entry_id", Entries)
+    val tag = reference("tag_id", Tags)
 }
