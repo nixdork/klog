fe91292ff2e410d21e5f44bf5d5fff165ffbfc2c
Jonas Gorauskas <jgorauskas@gmail.com>
2023-01-02 09:37:15 -0800

    extensions

--- src/main/kotlin/org/nixdork/klog/common/Extensions.kt
+++ src/main/kotlin/org/nixdork/klog/common/Extensions.kt
@@ -1,8 +1,10 @@
 package org.nixdork.klog.common
 
 import java.text.Normalizer
+import java.time.Instant
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
 import java.util.UUID
-import org.nixdork.klog.frameworks.data.config.properties.DatabaseProperties
 
 fun String.toUUID(): UUID = UUID.fromString(this)
 
@@ -13,9 +15,7 @@ fun String.slugify(): String = Normalizer.normalize(this, Normalizer.Form.NFD)
     .replace("\\s+".toRegex(), "-")
     .lowercase()
 
-// Klog Extensions
-fun DatabaseProperties.toJdbcUrl(): String {
-    val root = "jdbc:postgres://$host:$port/$name"
-    val paramsString = mapOf("prepareThreshold" to 0).map { (key, value) -> "$key=$value" }.joinToString("&")
-    return "$root?$paramsString"
-}
+// A `kotlinx.datetime.Instant`
+fun Instant.toOffsetDateTime(): OffsetDateTime = OffsetDateTime.ofInstant(this, ZoneOffset.UTC)
+
+
