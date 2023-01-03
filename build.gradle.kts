7c7085283a852f52ca3dcd9f9d93ee8cfa9d51aa
Yuri G <yurig3713@gmail.com>
2023-01-02 21:35:28 -0800

    test dependencies + fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,29 +22,40 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    // implementation("")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("org.flywaydb:flyway-core")
     implementation("com.zaxxer:HikariCP")
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("javax.validation:validation-api:2.0.1.Final")
+    implementation("org.flywaydb:flyway-core")
     implementation("org.jetbrains.exposed:exposed-core:0.41.1")
     implementation("org.jetbrains.exposed:exposed-dao:0.41.1")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.41.1")
     implementation("org.jetbrains.exposed:exposed-java-time:0.41.1")
-    implementation("javax.validation:validation-api:2.0.1.Final")
-
-    // implementation("")
-    implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
-    implementation("org.postgresql:postgresql:42.5.1")
-
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.41.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.postgresql:postgresql:42.5.1")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // testImplementation("")
+    testImplementation("com.ninja-squad:springmockk:4.0.0")
+    testImplementation("io.github.serpro69:kotlin-faker:1.13.0")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:1.3.4")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.5.4")
+    testImplementation("io.kotest:kotest-framework-datatest:5.5.4")
+    testImplementation("io.kotest:kotest-property:5.5.4")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.5.4")
+    testImplementation("io.mockk:mockk-jvm:1.13.3")
     testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.testcontainers:postgresql:1.17.6")
+    testImplementation("org.testcontainers:testcontainers:1.17.6")
 }
 
 tasks.withType<KotlinCompile> {
