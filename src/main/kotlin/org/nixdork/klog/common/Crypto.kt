f2b9d2fbd398d10a654057626b3104915fec1c43
Yuri G <yurig3713@gmail.com>
2023-01-13 19:52:40 -0800

    crypto functions

--- src/main/kotlin/org/nixdork/klog/common/Crypto.kt
+++ src/main/kotlin/org/nixdork/klog/common/Crypto.kt
@@ -3,14 +3,11 @@ package org.nixdork.klog.common
 import java.security.SecureRandom
 import java.util.HexFormat
 
-private const val SECURE_RANDOM_ALGORITHM = "NativePRNGNonBlocking"
-private const val BYTES_TO_GENERATE = 16
-
 fun ByteArray.hexEncode(): String = HexFormat.of().formatHex(this)
 
-fun generateSalt(): String {
-    val salt = ByteArray(BYTES_TO_GENERATE)
-    val srand = SecureRandom.getInstance(SECURE_RANDOM_ALGORITHM)
+fun generateSalt(size: Int = CRYPTO_BYTES_TO_GENERATE): String {
+    val salt = ByteArray(size)
+    val srand = SecureRandom.getInstance(CRYPTO_SECURE_RANDOM_ALGORITHM)
     srand.nextBytes(salt)
     return salt.hexEncode()
 }
