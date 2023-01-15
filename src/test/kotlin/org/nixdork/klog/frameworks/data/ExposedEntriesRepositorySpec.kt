e2f9ec75a8c270a8b0181b7927aa64037bdec13d
Yuri G <yurig3713@gmail.com>
2023-01-14 22:10:53 -0800

    entries db test

---
+++ src/test/kotlin/org/nixdork/klog/frameworks/data/ExposedEntriesRepositorySpec.kt
@@ -0,0 +1,20 @@
+package org.nixdork.klog.frameworks.data
+
+import io.kotest.core.spec.style.FunSpec
+import org.nixdork.klog.util.installPostgres
+
+class ExposedEntriesRepositorySpec : FunSpec({
+    installPostgres()
+
+    context("GET") {
+
+    }
+
+    context("UPSERT") {
+
+    }
+
+    context("DELETE") {
+
+    }
+})
