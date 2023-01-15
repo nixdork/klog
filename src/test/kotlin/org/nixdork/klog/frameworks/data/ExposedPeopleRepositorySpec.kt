dff119e5742c5b88cc52f18ac1ecc1a43474869e
Yuri G <yurig3713@gmail.com>
2023-01-14 23:04:28 -0800

    people db test

---
+++ src/test/kotlin/org/nixdork/klog/frameworks/data/ExposedPeopleRepositorySpec.kt
@@ -0,0 +1,20 @@
+package org.nixdork.klog.frameworks.data
+
+import io.kotest.core.spec.style.FunSpec
+import org.nixdork.klog.util.installPostgres
+
+class ExposedPeopleRepositorySpec : FunSpec({
+    installPostgres()
+
+    context("GET") {
+
+    }
+
+    context("UPSERT") {
+
+    }
+
+    context("DELETE") {
+
+    }
+})
