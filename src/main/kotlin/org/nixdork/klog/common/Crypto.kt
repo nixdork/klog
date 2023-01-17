06eace866128770847581ac9d695c1e31dc8f823
Yuri G <yurig3713@gmail.com>
2023-01-16 23:34:55 -0800

    crypto functions

--- src/main/kotlin/org/nixdork/klog/common/Crypto.kt
+++ src/main/kotlin/org/nixdork/klog/common/Crypto.kt
@@ -1,13 +1,19 @@
 package org.nixdork.klog.common
 
+import java.security.MessageDigest
 import java.security.SecureRandom
 import java.util.HexFormat
 
 fun ByteArray.hexEncode(): String = HexFormat.of().formatHex(this)
 
-fun generateSalt(size: Int = CRYPTO_BYTES_TO_GENERATE): String {
-    val salt = ByteArray(size)
-    val srand = SecureRandom.getInstance(CRYPTO_SECURE_RANDOM_ALGORITHM)
-    srand.nextBytes(salt)
-    return salt.hexEncode()
-}
+fun generateSalt(size: Int = CRYPTO_BYTES_TO_GENERATE): String =
+    ByteArray(size).also { salt ->
+        SecureRandom.getInstance(CRYPTO_SALT_RANDOM_ALGORITHM).apply {
+            nextBytes(salt)
+        }
+    }.hexEncode()
+
+fun generateHash(message: String, salt: String): String =
+    MessageDigest.getInstance(CRYPTO_HASH_RANDOM_ALGORITHM)
+        .digest((message + salt).toByteArray())
+        .hexEncode()
