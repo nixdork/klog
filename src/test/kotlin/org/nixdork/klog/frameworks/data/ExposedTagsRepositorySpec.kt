82d3c3ce46ca91f57b29aebf3b9126280677f39f
Yuri G <yurig3713@gmail.com>
2023-01-21 22:01:10 -0800

    tag db test tweaks

--- src/test/kotlin/org/nixdork/klog/frameworks/data/ExposedTagsRepositorySpec.kt
+++ src/test/kotlin/org/nixdork/klog/frameworks/data/ExposedTagsRepositorySpec.kt
@@ -1,30 +1,32 @@
 package org.nixdork.klog.frameworks.data
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.date.shouldHaveSameDayAs
 import io.kotest.matchers.ints.shouldBeExactly
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
-import java.time.OffsetDateTime
-import java.util.UUID
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.nixdork.klog.adapters.model.TagModel
 import org.nixdork.klog.frameworks.data.dao.Tags
 import org.nixdork.klog.util.createDatabaseTag
 import org.nixdork.klog.util.createKotlinTag
 import org.nixdork.klog.util.createRandomTag
 import org.nixdork.klog.util.faker
 import org.nixdork.klog.util.installPostgres
+import java.util.UUID
 
 class ExposedTagsRepositorySpec : FunSpec({
     installPostgres()
 
     lateinit var tagsRepo: ExposedTagsRepository
+    lateinit var r1Tag: TagModel
+    lateinit var r2Tag: TagModel
+    lateinit var dbTag: TagModel
+    lateinit var ktTag: TagModel
 
-    beforeEach {
+    beforeSpec {
         tagsRepo = ExposedTagsRepository()
-    }
-
-    context("GET") {
         (0..2).forEach {
             val tag = faker.createRandomTag()
             transaction {
@@ -32,96 +34,91 @@ class ExposedTagsRepositorySpec : FunSpec({
                     it[id] = tag.id
                     it[term] = tag.term
                     it[permalink] = tag.permalink
-                    it[createdAt] = OffsetDateTime.now().toInstant()
-                    it[updatedAt] = OffsetDateTime.now().toInstant()
                 }
             }
+            if (it == 1) r1Tag = tag
+            if (it == 2) r2Tag = tag
         }
-        val dbTag = faker.createDatabaseTag()
+        dbTag = faker.createDatabaseTag()
         transaction {
             Tags.insert {
                 it[id] = dbTag.id
                 it[term] = dbTag.term
                 it[permalink] = dbTag.permalink
-                it[createdAt] = OffsetDateTime.now().toInstant()
-                it[updatedAt] = OffsetDateTime.now().toInstant()
             }
         }
+        ktTag = faker.createKotlinTag()
+        transaction {
+            Tags.insert {
+                it[id] = ktTag.id
+                it[term] = ktTag.term
+                it[permalink] = ktTag.permalink
+            }
+        }
+    }
 
-        test("get all tags returns all tags") {
+    context("GET") {
+        test("get all returns all tags") {
             val tags = tagsRepo.getAllTags()
-            tags.tags.count() shouldBeExactly 4
+            tags.tags.count() shouldBeExactly 5
         }
 
-        test("get by valid term returns successfully") {
+        test("given existing term return tag") {
             val tag = tagsRepo.getTagByTerm("database")
             tag?.term shouldBe "database"
             tag?.entries?.count()?.shouldBeExactly(0)
         }
 
-        test("get by invalid term returns null") {
+        test("given a term that does not exist return null") {
             tagsRepo.getTagByTerm("random") shouldBe null
         }
 
-        test("get by valid id returns successfully") {
-            val tag = tagsRepo.getTagById(dbTag.id)
-            tag?.term shouldBe "database"
+        test("given existing id return tag") {
+            val tag = tagsRepo.getTagById(r1Tag.id)
+            tag?.term shouldBe r1Tag.term
             tag?.entries?.count()?.shouldBeExactly(0)
         }
 
-        test("get by invalid id returns null") {
+        test("given an id that does not exist return null") {
             tagsRepo.getTagById(UUID.randomUUID()) shouldBe null
         }
     }
 
     context("UPSERT") {
         test("update on pk conflict or insert otherwise") {
-            val ktTag = faker.createKotlinTag()
-            val inserted = tagsRepo.upsertTag(ktTag)
             val updated = tagsRepo.upsertTag(ktTag.copy(permalink = "gorauskas.org"))
             val current = tagsRepo.getTagByTerm("kotlin")!!
-            inserted.id shouldBe ktTag.id
-            inserted.term shouldBe ktTag.term
-            inserted.permalink shouldBe ktTag.permalink
             updated.id shouldBe current.id
             updated.term shouldBe current.term
             updated.permalink shouldBe current.permalink
-            // current.createdAt shouldBe inserted.createdAt
+            current.createdAt?.shouldHaveSameDayAs(updated.createdAt!!)
             current.updatedAt shouldNotBe null
         }
     }
 
     context("DELETE") {
+        test("delete tag given an existing id") {
+            tagsRepo.deleteTagById(r1Tag.id)
+            tagsRepo.getTagById(r1Tag.id) shouldBe null
+        }
 
+        test("do nothing given an id that does not exist") {
+            val id = UUID.randomUUID()
+            tagsRepo.getTagById(id) shouldBe null
+            tagsRepo.deleteTagById(id)
+            tagsRepo.getTagById(id) shouldBe null
+        }
+
+        test("delete tag given an existing term") {
+            tagsRepo.deleteTagByTerm(r2Tag.term)
+            tagsRepo.getTagByTerm(r2Tag.term) shouldBe null
+        }
+
+        test("do nothing given a term that does not exist") {
+            val rt = faker.createRandomTag()
+            tagsRepo.getTagByTerm(rt.term) shouldBe null
+            tagsRepo.deleteTagByTerm(rt.term)
+            tagsRepo.getTagByTerm(rt.term) shouldBe null
+        }
     }
 })
-
-//     override fun upsertTag(tag: TagModel): TagModel =
-//        transaction {
-//            Tags.upsert {
-//                it[id] = tag.id
-//                it[term] = tag.term
-//                it[permalink] = tag.permalink
-//            }.resultedValues!!
-//                .single()
-//                .let {
-//                    Tag.wrapRow(it).toModel()
-//                }
-//        }
-
-// object Tags : UUIDTable("tag") {
-//    val term = text("term")
-//    val permalink = text("permalink")
-//    val createdAt = timestamp("created_at")
-//    val updatedAt = timestamp("updated_at").nullable()
-//}
-
-// interface TagsRepository {
-//    fun getAllTags(): TagWrapperModel
-//    fun getTagByTerm(term: String): TagModel?
-//    fun getTagById(id: UUID): TagModel?
-//
-//    fun upsertTag(tag: TagModel): TagModel
-//
-//    fun deleteTag(tagId: UUID)
-// }
