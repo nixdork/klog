b634d5340d6d9d2337380d3350497aab15e94606
Yuri G <yurig3713@gmail.com>
2023-01-24 21:24:01 -0800

    isolation method

--- src/test/kotlin/org/nixdork/klog/KotestProjectConfig.kt
+++ src/test/kotlin/org/nixdork/klog/KotestProjectConfig.kt
@@ -2,8 +2,10 @@ package org.nixdork.klog
 
 import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.spec.IsolationMode
+import io.kotest.extensions.spring.SpringExtension
 
 object KotestProjectConfig : AbstractProjectConfig() {
     override val parallelism = 3
-    override val isolationMode = IsolationMode.SingleInstance
+    override val isolationMode = IsolationMode.InstancePerTest
+    override fun extensions() = listOf(SpringExtension)
 }
