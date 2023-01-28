722419dcbf709f475e87c1b8241d046fe96122d2
Jonas Gorauskas <jgorauskas@gmail.com>
2023-01-28 12:17:41 -0800

    using catalogs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,26 @@
+import org.gradle.api.internal.FeaturePreviews.Feature.TYPESAFE_PROJECT_ACCESSORS
+import org.gradle.api.internal.FeaturePreviews.Feature.VERSION_CATALOGS
+
+enableFeaturePreview(TYPESAFE_PROJECT_ACCESSORS.name)
+enableFeaturePreview(VERSION_CATALOGS.name)
+
 rootProject.name = "klog"
+
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        gradlePluginPortal()
+        mavenLocal()
+    }
+}
+
+gradle.rootProject {
+    val projectGroup: String by settings
+    val projectVersion: String by settings
+
+    allprojects {
+        group = projectGroup
+        version = projectVersion
+        description = "a bliki written in kotlin"
+    }
+}
