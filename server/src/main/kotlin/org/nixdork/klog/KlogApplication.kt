df4ac4b04350bd6f87f00a71e227102d73e2d7f5
Yuri G <yurig3713@gmail.com>
2023-01-31 07:41:42 -0800

    fix application boot run startup

--- server/src/main/kotlin/org/nixdork/klog/KlogApplication.kt
+++ server/src/main/kotlin/org/nixdork/klog/KlogApplication.kt
@@ -1,11 +1,20 @@
 package org.nixdork.klog
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
+import java.util.TimeZone
 
-@SpringBootApplication
+val DefaultTimeZone = TimeZone.getTimeZone("UTC")!!
+
+@SpringBootApplication(
+    scanBasePackages = ["org.nixdork.klog"],
+    exclude = [DataSourceAutoConfiguration::class],
+)
 class KlogApplication
 
+@Suppress("SpreadOperator")
 fun main(args: Array<String>) {
+    TimeZone.setDefault(DefaultTimeZone)
     runApplication<KlogApplication>(*args)
 }
